453e8c96333d69fbc696c40c0e5a1c2b4d430520
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2020-03-29 11:57:10 +0530

    Create prediction.py

---
+++ prediction.py
@@ -0,0 +1,13 @@
+#to generate the text character by character after training the model
+
+
+#importing necessary libraries
+
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+
+train_path='shakespeare.txt'
+
