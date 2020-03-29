1e2578198ff99690e3063ca71c8b91a7e47ae7cf
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2020-03-29 14:21:19 +0530

    loading trained moel for inference

--- models.py
+++ models.py
@@ -17,5 +17,26 @@ from tensorflow.keras.models import load_model
 
 #defining modelfunction
 
+def sparse_cat_loss(y_true,y_pred):
+  return sparse_categorical_crossentropy(y_true, y_pred, from_logits=True)
 
 
+
+def create_model(vocab_size, embed_dim, rnn_neurons, batch_size):
+    model = Sequential()
+
+    model.add(Embedding(vocab_size, embed_dim,batch_input_shape=[batch_size, None]))
+
+    model.add(GRU(rnn_neurons,return_sequences=True,stateful=True,recurrent_initializer='glorot_uniform'))
+
+    # Final Dense Layer to Predict
+    model.add(Dense(vocab_size))
+
+    model.compile(optimizer='adam', loss=sparse_cat_loss) 
+    
+    return model
+
+
+
+
+# print("model created successfully")
--- prediction.py
+++ prediction.py
@@ -7,9 +7,11 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import config as cf
+import models as md
 
 
 #importing tenserflow libraries
+import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM,Dense,Embedding,Dropout,GRU
 from tensorflow.keras.losses import sparse_categorical_crossentropy
@@ -17,7 +19,7 @@ from tensorflow.keras.models import load_model
 
 
 vocab= cf.vocab
-print(cf.length)
+#print(cf.length)
 
 #From the above output we can infer that the entire text corpus consists of 84 unique characters.
 
@@ -25,39 +27,22 @@ print(cf.length)
 # Let's create two dictionaries that can go from numeric index to character and character to numeric index.
 
 char_to_ind = {u:i for i, u in enumerate(vocab)}
-print(char_to_ind)
+#print(char_to_ind)
 
 #converting from index to character
 
 ind_to_char = np.array(vocab)
-print(ind_to_char)
-
-
-
-def sparse_cat_loss(y_true,y_pred):
-  return sparse_categorical_crossentropy(y_true, y_pred, from_logits=True)
-
-
-
-def create_model(vocab_size, embed_dim, rnn_neurons, batch_size):
-    model = Sequential()
-
-    model.add(Embedding(vocab_size, embed_dim,batch_input_shape=[batch_size, None]))
-
-    model.add(GRU(rnn_neurons,return_sequences=True,stateful=True,recurrent_initializer='glorot_uniform'))
-
-    # Final Dense Layer to Predict
-    model.add(Dense(vocab_size))
-
-    model.compile(optimizer='adam', loss=sparse_cat_loss) 
-    
-    return model
+#print(ind_to_char)
 
 
 #creating the model
-model = create_model(vocab_size, embed_dim, rnn_neurons, batch_size=1)
+model = md.create_model(cf.vocab_size, cf.embed_dim,cf. rnn_neurons, batch_size=1)
 
 model.load_weights(cf.model_path)
 
 model.build(tf.TensorShape([1, None]))
+
+print("model created successfully in prediction file and here is the summary of the model.....\n")
+
+print(model.summary())
 
