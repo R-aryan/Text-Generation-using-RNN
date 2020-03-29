73999bab4232170bf9d9c647c2c7477ceb71e85a
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2020-03-29 12:02:05 +0530

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -5,9 +5,28 @@
 
 import numpy as np
 import pandas as pd
-import tensorflow as tf
+#import tensorflow as tf
 import matplotlib.pyplot as plt
 
 
 train_path='shakespeare.txt'
+
+#reading the data from the text file
+text = open(train_path, 'r').read()
+print(text[:500])
+
+
+# The unique characters in the file
+vocab = sorted(set(text))
+print(vocab)
+length=len(vocab)
+print(length)
+
+#From the above output we can infer that the entire text corpus consists of 84 unique characters.
+
+#We know a neural network can't take in the raw string data,we need to assign numbers to each character. 
+# Let's create two dictionaries that can go from numeric index to character and character to numeric index.
+
+char_to_ind = {u:i for i, u in enumerate(vocab)}
+print(char_to_ind)
 
